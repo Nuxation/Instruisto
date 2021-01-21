f365d296bfa135de6a92b47bbdcfdc50ec1b74da
Nux <36564588+Nuxation@users.noreply.github.com>
2021-01-21 19:03:35 +0100

    Update AppFixtures.php
    Ajouts des fixtures pour les annonces

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -8,8 +8,10 @@ use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use App\Entity\User;
 use App\Entity\StatusAnnonce;
+use App\Entity\Presentiel;
 use App\Entity\Niveau;
 use App\Entity\Matiere;
+use App\Entity\Creneau;
 use App\Entity\Annonce;
 
 class AppFixtures extends Fixture
@@ -31,22 +33,23 @@ class AppFixtures extends Fixture
 
         //creation des Utilisateurs
         $diplome = ["Diplome A", "Diplome B", "Diplome C", "Diplome D"];
+        $users = [];
         for ($i = 0; $i < 5; $i++) {
-            $user = new User();
-            $user->setNom($faker->lastName)
+            $users[$i] = new User();
+            $users[$i]->setNom($faker->lastName)
                 ->setPrenom($faker->firstName)
                 ->setEmail($faker->firstName . '-' . $faker->lastName . '@etud.fr')
                 ->setTelephone($faker->phoneNumber())
                 ->setGenre($faker->randomElement(["Homme", "Femme"]))
                 //$encrypted = $this->passwordEncoder->encodePassword('test');
                 ->setPassword($this->passwordEncoder->encodePassword(
-                    $user,
+                    $users[$i],
                     'test'
                 ))
                 ->setDateDeNaissance(new \DateTime())
                 ->setPresentation($faker->paragraph)
                 ->setEtudeEtDiplome($diplome[array_rand($diplome)]);
-            $manager->persist($user);
+            $manager->persist($users[$i]);
         }
         $admin = new User();
         $admin->setNom("haspadar")
@@ -57,7 +60,7 @@ class AppFixtures extends Fixture
             ->setGenre("Homme")
             //$encrypted = $this->passwordEncoder->encodePassword('test');
             ->setPassword($this->passwordEncoder->encodePassword(
-                $user,
+                $admin,
                 'test'
             ))
             ->setDateDeNaissance(new \DateTime())
@@ -99,25 +102,56 @@ class AppFixtures extends Fixture
             $manager->persist($status[$i]);
         }
 
+        //Presentiel ou distanciel
+        $PresentielNom = [
+            "En Ligne", "En personne"
+        ];
+        $Presentiels = [];
+        for ($i = 0; $i < count($PresentielNom); $i++) {
+            $Presentiels[$i] = new Presentiel();
+            $Presentiels[$i]->setNom($PresentielNom[$i]);
+            $manager->persist($Presentiels[$i]);
+        }
+
 
         // Creation des annonces
         $lieux = ["Salon A", "Salon B", "Salon C", "Salon D"];
 
-        $anMatiere = $faker->randomElement($matieres);
-        $anNiveau = $faker->randomElement($niveaux);
-        $anStatusAnnonce = $faker->randomElement($status);
-        $annonce = new Annonce();
-        $annonce->setTitre("recherche un enseignant de {$anMatiere->getNom()}")
-            ->setDescription($faker->sentence($nbWords = 15, $variableNbWords = true))
-            ->setPrix($faker->numberBetween(5, 150))
-            ->setDureeEnMin($faker->numberBetween(10, 180))
-            ->setLieux($lieux[array_rand($lieux)])
-            ->setMatiere($anMatiere)
-            ->setNiveau($anNiveau)
-            ->setStatusAnnonce($anStatusAnnonce);
+        for ($i = 0; $i < 30; $i++) {
+            $anMatiere = $faker->randomElement($matieres);
+            $anNiveau = $faker->randomElement($niveaux);
+            $anStatusAnnonce = $faker->randomElement($status);
 
-        //$manager->persist($annonce);
+            $annonces[$i] = new Annonce();
+            // Creation de creaneau pour cette annonce
+            for ($y = 0; $y < $faker->numberBetween(1, 3); $y++) {
+                $Creneau = new Creneau();
+                $debutCourTimestamp = time() +  $faker->numberBetween($min = 60 * 60 * 24, $max = 60 * 60 * 24 * 14);
+                $debutCour = (new \DateTime())->setTimestamp($debutCourTimestamp);
+                $finCourTimestamp = $debutCourTimestamp +  $faker->numberBetween($min = 60 * 30, $max = 60 * 60 * 3);
+                $finCour = (new \DateTime())->setTimestamp($finCourTimestamp);
 
+                $Creneau
+                    ->setdebutAt($debutCour)
+                    ->setFinAt($finCour)
+                    ->setAnnonce($annonces[$i]);
+                $manager->persist($Creneau);
+            }
+
+            $annonces[$i]
+                ->setTitre("recherche un enseignant de {$anMatiere->getNom()}")
+                ->setDescription($faker->sentence($nbWords = 15, $variableNbWords = true))
+                ->setPrix($faker->numberBetween(5, 150))
+                ->setDureeEnMin($faker->numberBetween(10, 180))
+                ->setLieux($lieux[array_rand($lieux)])
+                ->setMatiere($anMatiere)
+                ->setNiveau($anNiveau)
+                ->setStatusAnnonce($anStatusAnnonce)
+                ->setAuteur($faker->randomElement($users))
+                ->setPresentiel($faker->randomElement($Presentiels));
+
+            $manager->persist($annonces[$i]);
+        }
         $manager->flush();
     }
 }
