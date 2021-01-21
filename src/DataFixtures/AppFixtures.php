ec0279dd438ab596ea7a36f7840a5812334a3be5
Nux <36564588+Nuxation@users.noreply.github.com>
2021-01-21 17:30:28 +0100

    Update AppFixtures.php
    Correction d'un bug dans les fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -7,6 +7,8 @@ use Faker\Factory;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use App\Entity\User;
+use App\Entity\StatusAnnonce;
+use App\Entity\Niveau;
 use App\Entity\Matiere;
 use App\Entity\Annonce;
 
@@ -81,26 +83,40 @@ class AppFixtures extends Fixture
         ];
         $niveaux = [];
         for ($i = 0; $i < count($niveauxNom); $i++) {
-            $niveaux[$i] = new Matiere();
+            $niveaux[$i] = new Niveau();
             $niveaux[$i]->setNom($niveauxNom[$i]);
             $manager->persist($niveaux[$i]);
         }
+
+        //creation des status d'Annonce
+        $statusNom = [
+            "recherche_enseignant", "enseignant_validé", "cours_fini"
+        ];
+        $status = [];
+        for ($i = 0; $i < count($statusNom); $i++) {
+            $status[$i] = new StatusAnnonce();
+            $status[$i]->setNom($statusNom[$i]);
+            $manager->persist($status[$i]);
+        }
+
 
         // Creation des annonces
         $lieux = ["Salon A", "Salon B", "Salon C", "Salon D"];
 
         $anMatiere = $faker->randomElement($matieres);
         $anNiveau = $faker->randomElement($niveaux);
+        $anStatusAnnonce = $faker->randomElement($status);
         $annonce = new Annonce();
-        /*$annonce->setTitre("recherche un enseignant de " + $anMatiere->getNom())
+        $annonce->setTitre("recherche un enseignant de {$anMatiere->getNom()}")
             ->setDescription($faker->sentence($nbWords = 15, $variableNbWords = true))
             ->setPrix($faker->numberBetween(5, 150))
             ->setDureeEnMin($faker->numberBetween(10, 180))
             ->setLieux($lieux[array_rand($lieux)])
             ->setMatiere($anMatiere)
-            ->setNiveau($anNiveau);
+            ->setNiveau($anNiveau)
+            ->setStatusAnnonce($anStatusAnnonce);
 
-        //$manager->persist($annonce);*/
+        //$manager->persist($annonce);
 
         $manager->flush();
     }
